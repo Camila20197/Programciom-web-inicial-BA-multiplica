6ea57332fedfef58784532dadd9041357e0b7e61
Camila20197 <camiladurand2097@gmail.com>
2023-11-23 20:34:15 -0300

    correccion en enviar_suscripcion.js

--- codigojs/enviar_subscripcion.js
+++ codigojs/enviar_subscripcion.js
@@ -1,10 +1,3 @@
-function redirectPage() {
-  var subscriptionValue = document.getElementById('btnSuscribirse');
-
-  if (subscriptionValue) {
-    alert("Gracias ya estás suscrito");
-  } else {
-    alert("Por favor, revisa tus datos");
-  }
-}
-
+document.getElementById('btnSuscribirse').addEventListener('click', function () {
+  alert("Gracias ya estás suscrito");
+});
